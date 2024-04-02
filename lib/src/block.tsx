60124d3855543c65db9e1fef97bea4f5b44c4002
motinados <motinados@gmail.com>
2024-04-03 05:55:13 +0900

    add block

---
+++ lib/src/block.tsx
@@ -0,0 +1,11 @@
+type Props = {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  fill: string;
+};
+
+export default function Block({ x, y, width, height, fill }: Props) {
+  return <rect x={x} y={y} width={width} height={height} fill={fill} />;
+}
