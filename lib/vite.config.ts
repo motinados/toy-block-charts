16bb95e97e7c58e25b6fccfc3d400c5aaa1ac58a
motinados <motinados@gmail.com>
2024-04-07 13:34:13 +0900

    Update vite.config.ts to exclude test and story files from dts plugin

--- lib/vite.config.ts
+++ lib/vite.config.ts
@@ -4,7 +4,7 @@ import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [react(), dts({ exclude: ["**/*.stories.ts", "**/*.test.ts"] })],
   build: {
     lib: {
       entry: "src/index.ts",
