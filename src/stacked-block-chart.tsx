4367d48a2063625d84ee930a336f850f94839033
motinados <motinados@gmail.com>
2024-04-09 04:59:21 +0900

    Consolidated the process of creating blocks into one

--- src/stacked-block-chart.tsx
+++ src/stacked-block-chart.tsx
@@ -1,48 +1,18 @@
 import { useMemo } from "react";
-import Block, { BlockItem } from "./block";
+import Block from "./block";
 import BlockLabels from "./block-labels";
 import Legend from "./legend";
 import {
-  calcWidthAndHeight,
-  calcPercentagesForData,
   getRandomColor,
   getRandomInt,
   shuffleArray,
+  getOrderdRandomInt,
+  calcHeight,
 } from "./utils";
 
-function datumToBlock(datum: DatumWithWidthHeight) {
-  return {
-    x: 0,
-    y: 0,
-    width: datum.width,
-    height: datum.height,
-    value: datum.value,
-    fill: datum.color,
-  };
-}
-
-function createBlocks(
-  data: DatumWithWidthHeight[],
-  svgCenterX: number
-): BlockItem[] {
-  const blocks = [];
-  let prevY = 0;
-  for (const datum of data) {
-    const block = datumToBlock(datum);
-    block.y = prevY;
-    prevY += block.height;
-
-    // Center the block
-    const fluctuation = getRandomInt(-10, 10);
-    block.x = svgCenterX - block.width / 2 + fluctuation;
-    blocks.push(block);
-  }
-  return blocks;
-}
-
 //Align BlockItem to the bottom of the svg based on svgHeight
-function alignToBottom(blocks: BlockItem[], svgHeight: number): BlockItem[] {
-  const resultBlocks: BlockItem[] = [];
+function alignToBottom(blocks: BlockDatum[], svgHeight: number): BlockDatum[] {
+  const resultBlocks: BlockDatum[] = [];
   const blocksHeight = blocks.reduce((acc, block) => acc + block.height, 0);
   const diff = svgHeight - blocksHeight;
 
@@ -60,6 +30,78 @@ export type Datum = {
   name: string;
   color?: string;
 };
+
+export type BlockDatum = {
+  value: number;
+  name: string;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  fill: string;
+  percentage: number;
+};
+
+/** Create initial BlockDatum */
+function createInitialBlockDatum(datum: Datum): BlockDatum {
+  return {
+    value: datum.value,
+    name: datum.name,
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0,
+    fill: datum.color || "",
+    percentage: 0,
+  };
+}
+
+/** If the color of BlockDatum is not set, set a random color */
+function setRandomColorIfNotSet(blockDatum: BlockDatum): BlockDatum {
+  if (!blockDatum.fill) {
+    return { ...blockDatum, fill: getRandomColor() };
+  }
+  return { ...blockDatum };
+}
+
+/** Calculate the percentage of BlockDatum */
+function setPercentage(blockDatum: BlockDatum, total: number): BlockDatum {
+  return { ...blockDatum, percentage: (blockDatum.value / total) * 100 };
+}
+
+/** Calculate the width and height of BlockDatum */
+function setWidthsAndHeights(
+  data: BlockDatum[],
+  multiple: number
+): BlockDatum[] {
+  const widths = getOrderdRandomInt(10, 100, data.length);
+  return data.map((datum, index) => ({
+    ...datum,
+    width: widths[index],
+    height: calcHeight(datum.percentage * multiple, widths[index]),
+  }));
+}
+
+/** Calculate x, y of BlockDatum */
+function calcBlocksPosition(
+  blocks: BlockDatum[],
+  svgCenterX: number
+): BlockDatum[] {
+  const resultBlocks: BlockDatum[] = [];
+  let prevY = 0;
+  for (const block of blocks) {
+    const newBlock = { ...block };
+
+    newBlock.y = prevY;
+    prevY += block.height;
+
+    const fluctuation = getRandomInt(-10, 10);
+    newBlock.x = svgCenterX - block.width / 2 + fluctuation;
+
+    resultBlocks.push(newBlock);
+  }
+  return resultBlocks;
+}
 
 export type DatumWithColor = Required<Datum>;
 export type DatumWithPercentage = DatumWithColor & { percentage: number };
@@ -87,16 +129,15 @@ export default function StackedBlockChart({
   const legendPaddingRight = 10;
 
   const { blocks, legendItems } = useMemo(() => {
-    const dataWithColor = data.map((d) => ({
-      ...d,
-      color: d.color || getRandomColor(),
-    }));
-
-    const dataWithPercentage = calcPercentagesForData(dataWithColor);
+    const initialBlocks = data.map(createInitialBlockDatum);
+    const dataWithColor = initialBlocks.map(setRandomColorIfNotSet);
+    const total = dataWithColor.reduce((acc, d) => acc + d.value, 0);
+    const dataWithPercentage = dataWithColor.map((d) =>
+      setPercentage(d, total)
+    );
     dataWithPercentage.sort((a, b) => a.percentage - b.percentage);
 
-    let dataWithWidthHeight = calcWidthAndHeight(dataWithPercentage, 100);
-
+    let dataWithWidthHeight = setWidthsAndHeights(dataWithPercentage, 100);
     if (type === "unstable-inverted") {
       dataWithWidthHeight.reverse();
     } else if (type === "shuffled") {
@@ -104,10 +145,13 @@ export default function StackedBlockChart({
     }
 
     const svgCenterX = (svgWidth - legendWidth) / 2 - 40;
-    const rawBlocks = createBlocks(dataWithWidthHeight, svgCenterX);
-    const blocks = alignToBottom(rawBlocks, svgHeight);
+    const dataWithPosition = calcBlocksPosition(
+      dataWithWidthHeight,
+      svgCenterX
+    );
+    const blocks = alignToBottom(dataWithPosition, svgHeight);
     const legendItems = dataWithWidthHeight.map((d) => {
-      return { name: d.name, color: d.color };
+      return { name: d.name, color: d.fill };
     });
 
     return { blocks, legendItems };
