64dc59ad0a5b9d2f32de05aeed9c4cce8713e903
motinados <motinados@gmail.com>
2024-04-09 05:23:32 +0900

    remove unused code

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,44 +1,6 @@
-import {
-  calcPercentages,
-  calcPercentagesForData,
-  getRandomInt,
-  shuffleArray,
-} from "./utils";
+import { getRandomInt, shuffleArray } from "./utils";
 
 describe("utils", () => {
-  it("calcPercentages", () => {
-    const array = [10, 20, 30, 40, 50];
-    const total = array.reduce((acc, cur) => acc + cur, 0);
-    const result = calcPercentages(array);
-    expect(result).toHaveLength(array.length);
-    expect(result).toEqual([
-      (10 / total) * 100,
-      (20 / total) * 100,
-      (30 / total) * 100,
-      (40 / total) * 100,
-      (50 / total) * 100,
-    ]);
-  });
-
-  it("calcPercentagesForData", () => {
-    const data = [
-      { value: 10, name: "A", color: "#000" },
-      { value: 20, name: "B", color: "#000" },
-      { value: 30, name: "C", color: "#000" },
-      { value: 40, name: "D", color: "#000" },
-      { value: 50, name: "E", color: "#000" },
-    ];
-    const result = calcPercentagesForData(data);
-    expect(result).toHaveLength(data.length);
-    expect(result).toEqual([
-      { value: 10, name: "A", percentage: (10 / 150) * 100, color: "#000" },
-      { value: 20, name: "B", percentage: (20 / 150) * 100, color: "#000" },
-      { value: 30, name: "C", percentage: (30 / 150) * 100, color: "#000" },
-      { value: 40, name: "D", percentage: (40 / 150) * 100, color: "#000" },
-      { value: 50, name: "E", percentage: (50 / 150) * 100, color: "#000" },
-    ]);
-  });
-
   it("getRandomInt", () => {
     const result = getRandomInt(10, 20);
     expect(result).toBeGreaterThanOrEqual(10);
--- src/utils.ts
+++ src/utils.ts
@@ -1,38 +1,5 @@
-import {
-  DatumWithColor,
-  DatumWithPercentage,
-  DatumWithWidthHeight,
-} from "./stacked-block-chart";
-
-export function calcPercentages(values: number[]) {
-  const total = values.reduce((acc, value) => acc + value, 0);
-  return values.map((value) => (value / total) * 100);
-}
-
-export function calcPercentagesForData(
-  data: DatumWithColor[]
-): DatumWithPercentage[] {
-  const values = data.map((d) => d.value);
-  const percentages = calcPercentages(values);
-  return data.map((d, index) => ({
-    ...d,
-    percentage: percentages[index],
-  }));
-}
-
 export function getRandomInt(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-export function calcRamdomRectDimensions(area: number): {
-  width: number;
-  height: number;
-} {
-  const ratioWidth = getRandomInt(1, 10);
-  const ratioHeight = getRandomInt(1, 10);
-  const height = Math.sqrt(area * (ratioHeight / ratioWidth));
-  const width = area / height;
-  return { width, height };
 }
 
 export function getRandomColor() {
@@ -48,23 +15,6 @@ export function getOrderdRandomInt(min: number, max: number, size: number) {
 // Calculate the height and return it by receiving the area and width
 export function calcHeight(area: number, width: number) {
   return area / width;
-}
-
-export function calcWidthAndHeight(
-  data: DatumWithPercentage[],
-  multiple: number
-): DatumWithWidthHeight[] {
-  const widths = getOrderdRandomInt(10, 100, data.length);
-  return data.map((datum, index) => ({
-    width: widths[index],
-    height: calcHeight(datum.percentage * multiple, widths[index]),
-    area: datum.percentage,
-    ...datum,
-  }));
-}
-
-export function makeSampleData(size: number) {
-  return Array.from({ length: size }, () => getRandomInt(1, 100));
 }
 
 // Receives an array and returns a shuffled array
