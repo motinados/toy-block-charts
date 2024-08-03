4114f1eca365e7311f01dc6022ab1909153ac5ba
motinados <motinados@gmail.com>
2024-08-03 17:39:20 +0900

    fix: shuffleArray returns the same array

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -7,7 +7,19 @@ describe("utils", () => {
     expect(result).toBeLessThanOrEqual(20);
   });
 
-  it("shuffleArray", () => {
+  it("should return the array as it is if the array has only one element", () => {
+    const arr = [1];
+    const result = shuffleArray(arr);
+    expect(result).toEqual(arr);
+  });
+
+  it("should return the array as it is if all elements are the same", () => {
+    const arr = [1, 1, 1, 1];
+    const result = shuffleArray(arr);
+    expect(result).toEqual(arr);
+  });
+
+  it("Return a shuffled array with shuffleArray", () => {
     const arr = [1, 2, 3, 4, 5];
     const result = shuffleArray(arr);
     expect(result).not.toEqual(arr);
